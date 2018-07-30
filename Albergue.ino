dbcc1292de103ed84b4d58cdf8dd5ce496edec13
Hugo <hugo_val28@hotmail.com>
2018-07-30 15:34:24 -0600

    change http request

--- Albergue.ino
+++ Albergue.ino
@@ -11,15 +11,19 @@
 #include <SPI.h>
 #include <ArduinoJson.h>
 
+//Relay variables
+int relay = 2;
+
 // Ethernet Variables
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-const char* host = "albergue-api.herokuapp.com";
-
+//const char* host = "albergue-api.herokuapp.com";
+byte server[] = { 192, 168, 1, 67 };
 IPAddress ip(192, 168, 1, 177);
 
 EthernetClient client;
 String result;
+String responseCode;
 
 //RFID Configuration
 #define RST_PIN  9    //Pin 9 para el reset del RC522
@@ -27,6 +31,7 @@ String result;
 MFRC522 mfrc522(SS_PIN, RST_PIN); ///Creamos el objeto para el RC522
 
 typedef struct {
+  int id;
   String codigo;
   String nombre;
 } Persona;
@@ -36,12 +41,12 @@ Persona personas[10];
 byte ActualUID[4]; //almacenará el código del Tag leído
 //RFID Users
 byte Usuario1[4] = {0xB4, 0xDC, 0x11, 0x71} ; //código del usuario 1
-byte Usuario2[4] = {0xC1, 0x2F, 0xD6, 0x0E} ; //código del usuario 2
+byte Usuario2[4] = {0x55, 0xF6, 0x1F, 0xA6} ; //código del usuario 2
 
 //Leds indicadores para la introduccion del codigo
-int ledCorrect = 23;
-int ledIncorrect = 25;
-int ledStatus = 27;
+int ledCorrect = 38;
+int ledIncorrect = 40;
+int ledStatus = 42;
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
@@ -59,15 +64,24 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup() {
   Serial.begin(9600);
-  EthernetConnection();
+//  EthernetConnection();
   pinMode(ledCorrect, OUTPUT);
   pinMode(ledIncorrect, OUTPUT);
   pinMode(ledStatus, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, HIGH);
   
-  personas[0] = Persona{ "1234", "Juan Perez" };
-  personas[1] = Persona{ "4321", "Hugo Licon" };
-
+//  personas[0] = Persona{1, "1234", "Juan Perez" };
+//  personas[1] = Persona{2, "4321", "Hugo Licon" };
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
+  Serial.println("connected");
+  Serial.println(Ethernet.localIP());
+  delay(1000);
   //RFID
   SPI.begin();        //Iniciamos el Bus SPI
   mfrc522.PCD_Init(); // Iniciamos el MFRC522  
@@ -83,30 +97,37 @@ void loop() {
       digitalWrite(ledStatus, HIGH);
       code = getCodeFromKeyPad();
 
-      Persona loggedUser = verifyUser(code);
+//      Persona loggedUser = verifyUser(code);
+//      200 = OK 404 = Not Found 401 = Usuario bloqueado
+      //hacer request para auth
+      auth();
       digitalWrite(ledStatus, LOW);
-      if (loggedUser.nombre.length() > 0) {
-        Serial.println("Iniciando Sesion");
-        postRegister();
-        print("Hola " + loggedUser.nombre);
+      if (responseCode.equals("200")) {
+        Serial.println("Sesión iniciada");
+//        print("Hola " + loggedUser.nombre);
         digitalWrite(ledCorrect, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(2000);
+        unlock();
+//        delay(2000);
         digitalWrite(ledCorrect, LOW);
-      } else {
+      } else if (responseCode.equals("404")){
         Serial.println("\nCódigo Incorrecto");
         Serial.println("Presione A para escribir código");
         digitalWrite(ledIncorrect, HIGH);   // turn the LED on (HIGH is the voltage level)
         delay(2000);
         digitalWrite(ledIncorrect, LOW);
+      } else if (responseCode.equals("401")) {
+        Serial.println("\n Usuario Bloqueado");
       }
     }
 
     if ( mfrc522.PICC_IsNewCardPresent())
     {
+      Serial.println("PICC_IsNewCardPresent");
       //Seleccionamos una tarjeta
       if ( mfrc522.PICC_ReadCardSerial())
       {
         // Enviamos serialemente su UID
+        Serial.println("Reading card...");
         Serial.print(F("Card UID:"));
         for (byte i = 0; i < mfrc522.uid.size; i++) {
           Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
@@ -117,17 +138,20 @@ void loop() {
         //comparamos los UID para determinar si es uno de nuestros usuarios
         if (compareArray(ActualUID, Usuario1)) {
           Serial.println("Acceso concedido...");
-          digitalWrite(LED_BUILTIN, HIGH);
-          postRegister();
-          delay(5000);
-          digitalWrite(LED_BUILTIN, HIGH);
+          digitalWrite(ledCorrect, HIGH);
+//          postRegister();
+          unlock();
+//          delay(5000);
+          digitalWrite(ledCorrect, LOW);
         }
         else if (compareArray(ActualUID, Usuario2))
         {
           Serial.println("Acceso concedido...");
-          digitalWrite(LED_BUILTIN, HIGH);
-          delay(5000);
-          digitalWrite(LED_BUILTIN, HIGH);
+          digitalWrite(ledCorrect, HIGH);
+//          postRegister();
+          unlock();
+//          delay(5000);
+          digitalWrite(ledCorrect, LOW);
         }
         else
           Serial.println("Acceso denegado...");
@@ -193,23 +217,28 @@ void print(String text) {
   Serial.println(text);
 }
 
-void postRegister(){
-  char data[]="{\"userId\": 1}";
+void auth(){
+  
+//  int userId = 1;
+//  String data = String("{\"userId\":") + userId + String("}");
+//  char data[]="{\"userId\": 1}";
   Serial.print("\n[Connecting to  ... ");
-  Serial.println(host);
-  if (client.connect(host, 80))
+//  Serial.println(host);
+//host 80
+  if (client.connect(server, 9000))
   {
     Serial.println("connected");
     Serial.println("[Sending a request]");
-    client.println("POST /registros HTTP/1.1");
-    client.println("Host: albergue-api.herokuapp.com");
+    client.println("GET /people/auth/1234 HTTP/1.1");
+//    client.println("Host: albergue-api.herokuapp.com");
+    client.println("Host: 192.168.1.67:9000");
     client.println("User-Agent: arduino/1.8.3");
-    client.println("Content-Type: application/json");
-    client.print("Content-Length: ");
-    client.println(strlen(data));
+//    client.println("Content-Type: application/json");
+//    client.print("Content-Length: ");
+//    client.println(data.length());
     client.println("Connection: close");
     client.println();
-    client.print(data);
+//    client.print(data);
 
     Serial.println("[Response:]");
     while (client.connected())
@@ -217,6 +246,10 @@ void postRegister(){
       if (client.available())
       {
         String line = client.readStringUntil('\n');
+        if(line.startsWith("HTTP/1.1")) {
+          responseCode = line.substring(9, 12);
+          Serial.println(responseCode);
+        }
         Serial.println(line);
       }
     }
@@ -251,3 +284,13 @@ void EthernetConnection(){
   Serial.println(Ethernet.localIP());
   delay(1000);
 }
+
+void unlock()
+{
+  digitalWrite(relay, LOW);
+  Serial.println("Abierto");
+  delay(3000);
+  digitalWrite(relay, HIGH);
+  Serial.println("Cerrada");
+}
+
